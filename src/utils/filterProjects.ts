510496861a8c24f77cedb31085f966fa25063683
Israel Larrondo <arsistyle@gmail.com>
2024-01-29 17:55:44 -0300

    add project detail

--- src/utils/filterProjects.ts
+++ src/utils/filterProjects.ts
@@ -3,13 +3,16 @@ import type { Langs, ProjectWithLang } from '../types';
 export const filterProjects = async ({
   projects = [],
   lang,
-  limit = -1,
+  limit = null,
 }: {
   projects: ProjectWithLang[];
   lang: Langs;
-  limit?: number;
+  limit?: number | null;
 }) => {
+
+  console.log(projects, lang, limit)
+
   const projectsByLang = projects.map((project) => project[lang]) || [];
-  const projectList = projectsByLang.slice(0, limit);
+  const projectList = limit ? projectsByLang.slice(0, limit) : projectsByLang;
   return projectList;
 };
